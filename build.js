04aabd9579907e6321e01aba1667d99871338a6f
Nathan Vaughn <nvaughn51@gmail.com>
2024-07-07 11:06:19 -0500

    Use loglevel instead of verbose [ci skip]

--- build.js
+++ build.js
@@ -1,17 +1,29 @@
-const spawn = require('cross-spawn');
+const spawn = require("cross-spawn");
 var base_url = "/";
 
-if (process.env.CF_PAGES_BRANCH === 'main') {
-    base_url = "https://pay.nathanv.me/";
+if (process.env.CF_PAGES_BRANCH === "main") {
+  base_url = "https://pay.nathanv.me/";
 } else if (process.env.CF_PAGES_URL) {
-    base_url = process.env.CF_PAGES_URL;
+  base_url = process.env.CF_PAGES_URL;
 }
 
 console.log(`Using base url "${base_url}"`);
-cmd = spawn.sync("npx", ["hugo", "--verbose", "--cleanDestinationDir", "--minify", "-b", base_url], { encoding: 'utf8' });
+cmd = spawn.sync(
+  "npx",
+  [
+    "hugo",
+    "--logLevel",
+    "info",
+    "--cleanDestinationDir",
+    "--minify",
+    "-b",
+    base_url,
+  ],
+  { encoding: "utf8" }
+);
 
 if (cmd.error) {
-    console.log("ERROR: ", cmd.error);
+  console.log("ERROR: ", cmd.error);
 }
 
 console.log(cmd.stdout);
